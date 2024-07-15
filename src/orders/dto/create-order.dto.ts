cb1a9ef24c6632d6a436be7cdcb92dcf5a135047
samir93bj <samir93bj@gmail.com>
2024-07-15 15:43:14 -0300

    add dto to create order

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,28 @@
-export class CreateOrderDto {}
+import { OrderStatus } from '@prisma/client';
+import { OrderStatusList } from '../enum/order.enum';
+import {
+  IsBoolean,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+} from 'class-validator';
+
+export class CreateOrderDto {
+  @IsNumber()
+  @IsPositive()
+  totalAmount: number;
+
+  @IsNumber()
+  @IsPositive()
+  totalItems: number;
+
+  @IsEnum(OrderStatusList, {
+    message: `Status must be one of the following values: ${OrderStatusList}`,
+  })
+  status = OrderStatus.PENDING;
+
+  @IsBoolean()
+  @IsOptional()
+  paid = false;
+}
